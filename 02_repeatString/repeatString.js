dde4755b8d99914711060a5d802cb550629fa85e
Tatien Miller <68832615+tatmil-99@users.noreply.github.com>
2021-10-26 18:04:04 -0500

    passes tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(str, num) {
+  let strToRepeat = [];
 
+  for (let i = 0; i < num; i++) {
+    strToRepeat.push(str);
+  }
+
+  if (num < 0) {
+    return 'ERROR';
+  } 
+  return strToRepeat.join('');
 };
 
 // Do not edit below this line
